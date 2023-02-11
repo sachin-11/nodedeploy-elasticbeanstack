26342a66b3e8db824d666e4259fad336b31f3716
sachin <sachin.singh@manektech.com>
2023-02-12 02:39:54 +0530

    init

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express();
 
 app.get('/demo', (req, res) => {
-    res.status(200).send('Hello this is secound deployment of elastic beanstack')
+    res.status(200).send('Hello this is secound deployment of elastic beanstack..')
 })
 
 const PORT = process.env.PORT || 3000
