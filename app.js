fe289f8a011911e1a9ddef10f10038cc5214f708
sachin <sachin.singh@manektech.com>
2023-02-11 20:06:19 +0530

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+
+
+const app = express();
+
+app.get('/demo', (req, res) => {
+    res.status(200).send('this is first deployment using elastic beanstack')
+})
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () => {
+    console.log(`server is listen on port ${PORT}`)
+})
